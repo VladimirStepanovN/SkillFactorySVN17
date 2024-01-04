3defe52e580c3884411e7a3538cc2147e5177e9b
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2024-01-05 02:14:39 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillFactorySVN17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
